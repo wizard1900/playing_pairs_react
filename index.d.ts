3dc42f55b2b5060e6a2fd2004b067bc2bdf3228d
wizard1900 <ilgas@yandex.ru>
2024-12-12 16:10:38 +0300

    web

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-// import { PluginOption } from "vite";
-
-declare module 'vite-plugin-css-modules' {
-  export function vitePluginCssModules(): string
-} 
--- src/Redux/store.ts
+++
@@ -1,9 +0,0 @@
-
-import { combineReducers, createStore } from "redux";
-import { startReducer } from "./start-reducer";
-import { fieldReducer } from "./field-reducer";
-
-const reducers = combineReducers({ startPage: startReducer, fieldPage: fieldReducer })
-export const store = createStore(reducers);
-
-
